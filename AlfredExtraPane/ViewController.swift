6f60ff329173d7f256f1815d615f8e08a4c7a855
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2020-11-24 10:00:00 +0530

    Hello world

---
+++ AlfredExtraPane/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  floating-webview
+//
+//  Created by Sujeet Gholap on 06/11/20.
+//  Copyright © 2020 Sujeet Gholap. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
