bf0a6070ad752a9b17d98153aacdb8b85585af0a
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2021-06-08 19:58:02 +0530

    Read config file path from command line

--- AlfredExtraPane/main.swift
+++ AlfredExtraPane/main.swift
@@ -4,12 +4,23 @@ import Foundation
 
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-  let pane = Pane(
-    config: PaneConfig(
-      alignment: .horizontal(placement: .right, width: 300, minHeight: 500),
-      workflowUID: "*"
-    )
+  let panes: [Pane]
+  let defaultConfig: PaneConfig = PaneConfig(
+    alignment: .horizontal(placement: .right, width: 300, minHeight: 400),
+    workflowUID: "*"
   )
+
+  init(_ configFilePath: URL?) {
+    var configs: [PaneConfig] = []
+    if let configFilePath = configFilePath {
+      configs = read(contentsOf: configFilePath) ?? []
+    }
+    if configs.isEmpty {
+      log("Loading default catchall-config")
+      configs = [defaultConfig]
+    }
+    panes = configs.map { Pane(config: $0) }
+  }
 
   func application(_ application: NSApplication, open urls: [URL]) {
     for url in urls {
@@ -21,7 +32,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 autoreleasepool {
   let app = NSApplication.shared
-  let delegate = AppDelegate()
+  var configFilePath: URL? = nil
+  if let configPath: String = CommandLine.arguments.suffix(from: 1).first {
+    configFilePath = URL(fileURLWithPath: configPath)
+  } else {
+    log("No config file path provided.")
+  }
+  let delegate = AppDelegate(configFilePath)
   app.setActivationPolicy(.accessory)
   app.delegate = delegate
   app.run()
