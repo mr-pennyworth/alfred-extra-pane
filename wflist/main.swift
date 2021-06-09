01b8efd9dcd0cee09276b19575bbae2f59319d4d
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2021-06-09 07:49:16 +0530

    Print workflow list in script filter JSON format in wflist

--- wflist/main.swift
+++ wflist/main.swift
@@ -1,4 +1,19 @@
+import Alfred
 import Foundation
 
-print("{\"items\": []}")
+let wflist = ScriptFilterResponse(
+  items: Alfred.workflows().map { wf in
+    ScriptFilterResponse.Item.item(
+      arg: wf.uid,
+      title: wf.name,
+      uid: wf.uid,
+      icon: .fromImage(at: wf.dir/"icon.png")
+    )
+  }
+)
+
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+let encoded = try! encoder.encode(wflist)
+print(String(data: encoded, encoding: .utf8)!)
 
